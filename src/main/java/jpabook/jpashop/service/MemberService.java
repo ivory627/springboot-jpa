199c0dcad8ba58cbb25a1b53b6b99316c071e614
ivory627 <ivory627@naver.com>
2024-06-13 16:35:23 +0900

    feat : 회원 도메인 개발
    - 회원 리포지토리 개발
    - 회원 서비스 개발 (회원가입, 조회, 중복검사)
    - 회원 기능 테스트

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,59 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor //생성자 주입
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     * @param member
+     * @return member id
+     */
+    @Transactional
+    public Long join(Member member) {
+
+        //중복 회원 검증
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+
+        if(!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     * @return member list
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 단건 조회
+     * @param memberId
+     * @return member
+     */
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+
+}
