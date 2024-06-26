c880387d9c043d55b83572a07a0866f768111ced
ivory627 <ivory627@naver.com>
2024-06-27 01:53:49 +0900

    feat : 웹계층 개발
    - 홈화면, 기본 레이아웃 개발
    - 회원 등록, 조회
    - 상품 등록, 조회, 수정
    - 상품 주문, 주문 목록 조회, 검색, 취소

--- src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -17,9 +17,16 @@ public class ItemRepository {
         if(item.getId() == null) {
             em.persist(item);
         }else {
+            /*
+                병합 : 준영속 상태의 엔티티를 영속 상태로 변경
+                - 영속엔티티의 모든 필드를 준영속 엔티티 값으로 교체함
+                - 값이 없으면 null 값으로 변경되는 위험이 있음
+             */
             em.merge(item);
         }
     }
+
+
 
     public Item findOne(Long id) {
         return em.find(Item.class, id);
--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -28,4 +28,15 @@ public class ItemService {
     public Item findOne(Long itemId) {
         return itemRepository.findOne(itemId);
     }
+
+    /**
+     * 변경감지 (=dirty checking) : 영속성 컨텍스트가 자동 변경. 병합보다 해당 방법을 권장함
+     */
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item item = itemRepository.findOne(itemId);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
+    }
 }
