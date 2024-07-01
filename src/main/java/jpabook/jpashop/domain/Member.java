acf964441f005988b905b6f34b22b0e3b01bd7be
ivory627 <ivory627@naver.com>
2024-07-01 17:53:59 +0900

    feat : API 개발
    - 회원 등록 API
    - 회원 수정 API
    - 회원 조회 API

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,11 +17,13 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    //@NotEmpty : DTO 활용할것
     private String name;
 
     @Embedded
     private Address address;
 
+    //@JsonIgnore : DTO 활용할것, 하나의 엔티티로 다양한 api가 개발되므로 화면에 종속적이면 안된다.
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 }
